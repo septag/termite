e089f01da3edb615a22e75a48024559b54ec36dd
septag <sep.tagh@gmail.com>
2018-06-03 16:45:23 +0430

    iOS test #1

--- source/termite/ios_window.mm
+++ source/termite/ios_window.mm
@@ -2,6 +2,7 @@
 #include <AVFoundation/AVFoundation.h>
 #include "tmath.h"
 #include <mach/mach_host.h>
+#include <SDL_uikitappdelegate.h>
 
 void* iosCreateNativeLayer(void* wnd)
 {
@@ -35,3 +36,17 @@ uint8_t iosGetCoreCount()
 {
     return [[NSProcessInfo processInfo] processorCount];
 }
+
+@implementation SDLUIKitDelegate(MyMethods)
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity
+                    restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler
+{
+    if (userActivity.activityType == NSUserActivityTypeBrowsingWeb) {
+        NSURL* url = userActivity.webpageURL;
+        NSString* s = [url absoluteString];
+        const char* curl = [s UTF8String];
+        puts(curl);
+    }
+    return TRUE;
+}
+@end
